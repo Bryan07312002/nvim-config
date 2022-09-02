37b633fb8aa20141500d385a2cbe1be9831de521
Bryan <contato.bryanbecker@gmail.com>
2022-09-02 00:46:30 -0300

    feat: new mappings

--- lua/configs/mappings.lua
+++ lua/configs/mappings.lua
@@ -16,15 +16,23 @@ vim.cmd([[ autocmd BufNewFile *.sh :call append(0, '#!/usr/bin/env bash') ]])
 -- Se não existir o arquivo C++ de extensão .cpp ele preenche da forma abaixo
 -- altere para sua(s) linguagens preferidas
 vim.cmd([[
-	function! AutoCpp()
-	  call append(0, '#include <iostream>')
-	  call append(1, '')
-	  call append(2, 'int main( int argc , char **argv ){')
-	  call append(3, "  std::cout << \"Olá, Mundo!\" << '\\n';")
-	  call append(4, '  return 0;')
-	  call append(5, '}')
-	  call cursor(4, 17)
-	endfunction
-	autocmd BufNewFile *.cpp :call AutoCpp()
+  function! AutoCpp()
+    call append(0, '#include <iostream>')
+    call append(1, '')
+    call append(2, 'int main( int argc , char **argv ){')
+    call append(3, "  std::cout << \"Olá, Mundo!\" << '\\n';")
+    call append(4, '  return 0;')
+    call append(5, '}')
+    call cursor(4, 17)
+  endfunction
+  autocmd BufNewFile *.cpp :call AutoCpp()
 ]])
 
+--Change buffer
+vim.cmd([[ nnoremap <C-l> :bn<CR> ]])
+vim.cmd([[ nnoremap <C-h> :bf<CR> ]])
+
+--Close buffer
+vim.cmd([[ nnoremap <C-k> :bdelete<CR> ]])
+
+-- Telescope
